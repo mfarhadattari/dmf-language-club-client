dbe50e81217a2595755e90bedabff580b8ed8444
Mohammad Farhad <mfarhad.attari@gmail.com>
2023-06-07 20:46:33 +0600

    create user function and get logged user

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,5 +1,9 @@
-import { createContext, useState } from "react";
-import { getAuth } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+} from "firebase/auth";
 import app from "./../Firebase/firebase.config";
 
 export const AuthContext = createContext(null);
@@ -8,11 +12,35 @@ const AuthProvider = ({ children }) => {
   const [authUser, setAuthUser] = useState(null);
   const [authLoading, setAuthLoading] = useState(true);
 
+  // !------------ CREATE USER -------------------! //
+  const createUser = (email, password) => {
+    setAuthLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  
+  // !----------- GET Logged User ---------------! //
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      if (currentUser) {
+        setAuthUser(currentUser);
+        setAuthLoading(false);
+      } else {
+        setAuthUser(null);
+        setAuthLoading(false);
+      }
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
   /* --------------------------------------------------
   !------------------ AUTH INFO ---------------------- */
   const authInfo = {
     authUser,
     authLoading,
+    createUser,
   };
 
   return (
