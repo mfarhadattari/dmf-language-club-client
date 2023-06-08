eb0014a345be3039684e70ca72e3ebb94c5bbe4e
Mohammad Farhad <mfarhad.attari@gmail.com>
2023-06-08 15:41:15 +0600

    popular class load from server

--- src/pages/public/Home/Section/PopularClasses.jsx
+++ src/pages/public/Home/Section/PopularClasses.jsx
@@ -1,14 +1,25 @@
+import { useState } from "react";
 import SectionHeading from "../../../../components/SectionHeading";
+import useAxios from "./../../../../hooks/useAxios";
 
 const PopularClasses = () => {
-    return (
-        <section className="my-10">
-            <SectionHeading heading="Popular Classes"></SectionHeading>
-            <div>
-                
-            </div>
-        </section>
-    );
+  const { axiosReq } = useAxios();
+  const [popularClasses, setPopularClasses] = useState([]);
+
+  axiosReq.get("/popular-classes").then(({ data }) => {
+    setPopularClasses(data);
+  });
+
+  return (
+    <section className="my-10">
+      <SectionHeading heading="Popular Classes"></SectionHeading>
+      <div>
+        {
+        popularClasses.map(item => <p key={item._id}>{item.name}</p> )
+        }
+        </div>
+    </section>
+  );
 };
 
 export default PopularClasses;
