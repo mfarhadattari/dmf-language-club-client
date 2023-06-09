44d9aa2ef226bc311195d32fed6b1bb93f769be0
Mohammad Farhad <mfarhad.attari@gmail.com>
2023-06-09 10:42:08 +0600

    load all instructors

--- src/pages/public/Instuctors/Instructors.jsx
+++ src/pages/public/Instuctors/Instructors.jsx
@@ -1,11 +1,28 @@
+import { useState } from "react";
 import Cover from "./../../../components/Cover";
+import { useEffect } from "react";
+import useAxios from "../../../hooks/useAxios";
+import Loaders from "../../../components/Loaders";
 const Instructors = () => {
+  const { axiosReq } = useAxios();
+  const [instructors, setInstructors] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    axiosReq.get("/all-instructor").then(({ data }) => {
+      setInstructors(data);
+      setLoading(false);
+    });
+  }, [axiosReq]);
+
   return (
     <main>
       <Cover
         heading="Our Instructors"
-        bgImage="bg-[url('https://i.ibb.co/j3jx98j/teacher-doing-english-lesson-online-his-students-23-2148963016.jpg')]"
+        bgImage="bg-[url('https://i.ibb.co/zFqd4M5/everyone-is-smiling-listens-group-people-business-conference-modern-classroom-daytime-146671-16288.jpg')]"
       ></Cover>
+      {loading ? <Loaders></Loaders> : <div>{instructors.length}</div>}
     </main>
   );
 };
