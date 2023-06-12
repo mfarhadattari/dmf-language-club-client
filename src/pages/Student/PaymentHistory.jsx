8c4b20635ef15f751f5eb4dcd08ba4f20c4cf265
Mohammad Farhad <mfarhad.attari@gmail.com>
2023-06-12 21:02:31 +0600

    payment history data load

--- src/pages/Student/PaymentHistory.jsx
+++ src/pages/Student/PaymentHistory.jsx
@@ -1,6 +1,22 @@
+import { useEffect, useState } from "react";
 import SetTitle from "../../components/setTitle";
+import useAuthContext from "./../../hooks/useAuthContext";
+import useSecureAxios from "./../../hooks/useSecureAxios";
 
 const PaymentHistory = () => {
+  const { authUser } = useAuthContext();
+  const { secureAxios } = useSecureAxios();
+  const [payments, setPayments] = useState([]);
+
+  useEffect(() => {
+    secureAxios
+      .get(`/student/payment-history?email=${authUser?.email}`)
+      .then(({ data }) => {
+        setPayments(data);
+      });
+  }, [authUser, secureAxios]);
+
+  console.log(payments);
   return (
     <main>
       <SetTitle title="Payment History - DMF Language Club"></SetTitle>
