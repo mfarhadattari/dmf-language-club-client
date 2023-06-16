6dfd60c468aff56671f5ebbdb19390b6e29af23d
Mohammad Farhad <mfarhad.attari@gmail.com>
2023-06-16 12:39:22 +0600

    Click handler added for update and edit btn remove from every input and add one only

--- src/pages/public/Account/Profile.jsx
+++ src/pages/public/Account/Profile.jsx
@@ -4,6 +4,7 @@ import useAuthContext from "./../../../hooks/useAuthContext";
 import useSecureAxios from "./../../../hooks/useSecureAxios";
 import Loaders from "./../../../components/Loaders";
 import { FaEdit } from "react-icons/fa";
+import SecondaryBtn from "./../../../components/Button/SecondaryBtn";
 
 const Profile = () => {
   const { authUser } = useAuthContext();
@@ -23,7 +24,11 @@ const Profile = () => {
     },
   });
 
-  console.log(profileInfo);
+  const handelUpdateProfile = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    console.log(form);
+  };
 
   return (
     <main>
@@ -36,29 +41,34 @@ const Profile = () => {
             <h1 className="text-3xl my-2 text-center font-semibold">
               Profile Information
             </h1>
-            <div className="flex w-full">
-              <div className="w-1/3 flex flex-col items-center gap-5">
-                <div className="avatar">
-                  <div className="w-32 h-32 rounded-full">
-                    <img src={profileInfo?.photoURL} />
+            <form className="flex w-full" onSubmit={handelUpdateProfile}>
+              <div className="w-1/3 relative">
+                <button
+                  type="button"
+                  className="btn btn-circle btn-outline text-blue-600 absolute top-0 text-xl "
+                >
+                  <FaEdit></FaEdit>
+                </button>
+                <div className="flex flex-col items-center gap-5">
+                  <div className="avatar">
+                    <div className="w-32 h-32 rounded-full">
+                      <img src={profileInfo?.photoURL} />
+                    </div>
                   </div>
-                </div>
-                <div className="form-control gap-2">
-                  <div className="flex gap-5 items-center">
-                    <input
-                      type="url"
-                      placeholder="Photo URL"
-                      disabled
-                      defaultValue={profileInfo.photoURL}
-                      className="input input-bordered w-full border-blue-600 focus:outline-blue-600"
-                    />
-                    <button type="button" className="text-2xl">
-                      <FaEdit></FaEdit>
-                    </button>
+                  <div className="form-control gap-2">
+                    <div className="flex gap-5 items-center">
+                      <input
+                        type="url"
+                        placeholder="Photo URL"
+                        disabled
+                        defaultValue={profileInfo.photoURL}
+                        className="input input-bordered w-full border-blue-600 focus:outline-blue-600"
+                      />
+                    </div>
                   </div>
                 </div>
               </div>
-              <form className="card-body w-3/4">
+              <div className="card-body w-3/4">
                 <div className="form-control gap-2 w-full">
                   <div className="flex gap-2 w-full items-center">
                     <input
@@ -68,9 +78,6 @@ const Profile = () => {
                       defaultValue={profileInfo.displayName}
                       className="input input-bordered w-full text-xl border-blue-600 focus:outline-blue-600"
                     />
-                    <button type="button" className="text-2xl">
-                      <FaEdit></FaEdit>
-                    </button>
                   </div>
                 </div>
                 <div className="form-control gap-2 w-full">
@@ -82,9 +89,6 @@ const Profile = () => {
                       defaultValue={profileInfo.email}
                       className="input input-bordered w-full text-xl border-blue-600 focus:outline-blue-600"
                     />
-                    <button type="button" className="text-2xl">
-                      <FaEdit></FaEdit>
-                    </button>
                   </div>
                 </div>
                 <div className="form-control gap-2 w-full">
@@ -96,9 +100,6 @@ const Profile = () => {
                       defaultValue={profileInfo?.phone}
                       className="input input-bordered w-full text-xl border-blue-600 focus:outline-blue-600"
                     />
-                    <button type="button" className="text-2xl">
-                      <FaEdit></FaEdit>
-                    </button>
                   </div>
                 </div>
                 <div className="form-control gap-2 w-full">
@@ -112,9 +113,6 @@ const Profile = () => {
                       <option value="female">Female</option>
                       <option value="male">Male</option>
                     </select>
-                    <button type="button" className="text-2xl">
-                      <FaEdit></FaEdit>
-                    </button>
                   </div>
                 </div>
                 <div className="form-control gap-2 w-full">
@@ -127,13 +125,13 @@ const Profile = () => {
                       defaultValue={profileInfo?.address}
                       className="border rounded-lg p-3 w-full text-xl border-blue-600 focus:outline-blue-600"
                     ></textarea>
-                    <button type="button" className="text-2xl">
-                      <FaEdit></FaEdit>
-                    </button>
                   </div>
                 </div>
-              </form>
-            </div>
+                <div className="form-control gap-2 w-full">
+                  <SecondaryBtn>Update</SecondaryBtn>
+                </div>
+              </div>
+            </form>
           </div>
         </section>
       )}
