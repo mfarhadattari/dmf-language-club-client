7758fcfe16ad6d0387a47ac9e6fcd445ed0bfee9
Mohammad Farhad <mfarhad.attari@gmail.com>
2023-06-16 11:23:58 +0600

    get profile info of an user

--- src/pages/public/Account/Profile.jsx
+++ src/pages/public/Account/Profile.jsx
@@ -1,9 +1,33 @@
+import { useQuery } from "@tanstack/react-query";
 import SetTitle from "../../../components/SetTitle";
+import useAuthContext from "./../../../hooks/useAuthContext";
+import useSecureAxios from "./../../../hooks/useSecureAxios";
+import Loaders from "./../../../components/Loaders";
 
 const Profile = () => {
+  const { authUser } = useAuthContext();
+  const { secureAxios } = useSecureAxios();
+
+  const {
+    data: profileInfo = {},
+    isLoading,
+    refetch: refetchProfileInfo,
+  } = useQuery({
+    queryKey: ["profileInfo", secureAxios, authUser],
+    queryFn: async () => {
+      const res = await secureAxios.get(
+        `/user-profile?email=${authUser?.email}`
+      );
+      return res.data;
+    },
+  });
+
+  console.log(profileInfo);
+
   return (
     <main>
       <SetTitle title="Profile - DMF Language Club"></SetTitle>
+      {isLoading ? <Loaders></Loaders> : <section></section>}
     </main>
   );
 };
